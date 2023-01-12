5737109fd4d9697c9b573c647c69c52a2de8c56b
Sam Wynne <sam.wynne@snapraise.com>
2023-01-12 17:12:06 -0600

    Tidying component/section imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import Navbar from "../components/Navbar";
-import About from "../components/About";
-import Explore from "../components/Explore";
-import Feedback from "../components/Feedback";
-import Footer from "../components/Footer";
-import GetStarted from "../components/GetStarted";
-import Hero from "../components/Hero";
-import Insights from "../components/Insights";
-import WhatsNew from "../components/WhatsNew";
-import World from "../components/World";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 export default function Home() {
   return (
