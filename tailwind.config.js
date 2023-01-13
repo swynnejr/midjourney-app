8fe74ab7c9a425124325eda81f97ea2f7426cdfb
Sam Wynne <sam.wynne@snapraise.com>
2023-01-12 22:07:44 -0600

    Updated for appDir

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
   ],
   content: [],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-black": "#1A232E",
+        "secondary-white": "#c7c7c7",
+      },
+      transitionTimingFunction: {
+        "out-flex": "cubic-bezier(0.05, 0.6, 0.4, 0.9)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
