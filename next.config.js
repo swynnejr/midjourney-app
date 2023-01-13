dae29c0820897e99ea923d2e1a1a19b150515af8
Sam Wynne <sam.wynne@snapraise.com>
2023-01-12 22:57:36 -0600

    Hero Art

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ['cdn.midjourney.com'],
+  },
 }
 
 module.exports = nextConfig
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,11 +1,62 @@
-'use client';
+"use client";
 
-type Props = {}
+import { motion } from "framer-motion";
+import Image from "next/image";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
+type Props = {};
 
 function Hero({}: Props) {
   return (
-    <div>Hero</div>
-  )
+    <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <div className="flex justify-center items-center flex-col relative z-10">
+          <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+            Midjourney
+          </motion.h1>
+          <motion.div
+            variants={textVariant(1.2)}
+            className="flex flex-row justify-center items-center"
+          >
+            <h1 className={styles.heroHeading}>Ma</h1>
+            <div className={styles.heroDText} />
+            <h1 className={styles.heroHeading}>Ness</h1>
+          </motion.div>
+        </div>
+        <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
+        <Image 
+          src="https://cdn.midjourney.com/8f72b394-eddf-4b5e-b9d2-cebbef59c732/grid_0.png"
+          className="w-full object-cover rounded-tl-[140px] z-10 relative"
+          alt="Astronaut Drowning in a Giant Aquarium"
+          width={800}
+          height={800}
+        />
+
+        <a href="#explore">
+          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+            {/* <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+            /> */}
+          </div>
+        </a>
+      </motion.div>
+      </motion.div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
