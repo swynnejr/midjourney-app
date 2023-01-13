c97e5249a8139d10fd44e8dce4dff5907b0b2057
Sam Wynne <sam.wynne@snapraise.com>
2023-01-12 22:14:39 -0600

    Title update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-
 import styles from "../styles";
 import { navVariants } from "../utils/motion";
 import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/react/24/solid";
@@ -22,7 +21,7 @@ function Navbar({}: Props) {
       >
         <MagnifyingGlassIcon className="w-[24px] h-[24px] object-contain text-white" />
         <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
-          METAVERSUS
+          AI Generated Art
         </h2>
         <Bars3Icon className="w-[24px] h-[24px] object-contain text-white" />
       </div>
