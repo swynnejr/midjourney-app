18498d69ef20f94cc867d263218fb9986f910361
Sam Wynne <sam.wynne@snapraise.com>
2023-01-12 22:08:30 -0600

    updated for appDir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./sections/**/*.{js,ts,jsx,tsx}",
     "./styles/**/*.{js,ts,jsx,tsx}",
   ],
-  content: [],
   mode: "jit",
   theme: {
     extend: {
