7aeaadde3e4efc3acd0240c0241327b4bed43e02
Sam Wynne <saucersam@gmail.com>
2023-01-15 17:45:20 -0600

    Adding StartSteps component with types

---
+++ components/StartSteps.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import styles from '../styles'
+
+type Props = {
+  number: React.ReactNode,
+  text: React.ReactNode
+}
+
+export default function StartSteps({number, text}: Props) {
+  return (
+    <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      <p className="font-bold text-[20px] text-white">
+        {number}
+      </p>
+    </div>
+    <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {text}
+    </p>
+  </div>
+  )
+}
